60bf9a5b3468a9e3f69b1c337c70d7af768e1ed0
WCalderbank <wcalderbank99@gmail.com>
2022-03-17 21:50:44 +0000

    Linked up

---
+++ creative-website/src/components/MainSection.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { Button } from "./Button";
+import "../App.css";
+import "./MainSection.css";
+import cover from "../components/vids/static.mp4";
+
+function MainSection() {
+  return (
+    <div className="main-container">
+      <video autoPlay loop muted>
+        <source src={cover} type="video/mp4" />
+      </video>
+      <h1 className="main-title">Explore Everything</h1>
+      <h3 className="main-sub">Go Full Geek</h3>
+      <div className="main-btns">
+        <Button
+          className="btns"
+          buttonStyle="btn--outline"
+          buttonSize="btn--large"
+        >
+          Lets Go
+        </Button>
+        <Button
+          className="btns"
+          buttonStyle="btn--primary"
+          buttonSize="btn--large"
+        >
+          Watch <i className="fas fa-play-circle" />
+        </Button>
+      </div>
+    </div>
+  );
+}
+
+export default MainSection;
---
+++ creative-website/src/components/Navbar.js
@@ -0,0 +1,71 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { Button } from "./Button";
+import "./Navbar.css";
+
+function Navbar() {
+  const [click, setClick] = useState(false);
+  const [button, setButton] = useState(true);
+
+  const handleClick = () => setClick(!click);
+  const closeMenu = () => setClick(false);
+
+  const showButton = () => {
+    if (window.innerWidth <= 960) {
+      setButton(false);
+    } else {
+      setButton(true);
+    }
+  };
+
+  window.addEventListener("resize", showButton);
+
+  return (
+    <>
+      <nav className="navbar">
+        <div className="navbar-container">
+          <Link to="/" className="navbar-logo">
+            NerdNetwork <i className=" fas fa-glasses" />
+          </Link>
+          <div className="menu-icon" onClick={handleClick}>
+            <i className={click ? "fas fa-times" : "fas fa-bars"} />
+          </div>
+          <ul className={click ? "nav-menu active" : "nav-menu"}>
+            <li className="nav-options">
+              <Link to="/film&tv" className="nav-links" onClick={closeMenu}>
+                Film & Television
+              </Link>
+            </li>
+            <li className="nav-options">
+              <Link to="/videogames" className="nav-links" onClick={closeMenu}>
+                Videogames
+              </Link>
+            </li>
+            <li className="nav-options">
+              <Link to="/books" className="nav-links" onClick={closeMenu}>
+                Books
+              </Link>
+            </li>
+            <li className="nav-options">
+              <Link to="/news" className="nav-links" onClick={closeMenu}>
+                NerdNews
+              </Link>
+            </li>
+            <li className="nav-options">
+              <Link
+                to="/log-in"
+                className="nav-links-mobile"
+                onClick={closeMenu}
+              >
+                Log In
+              </Link>
+            </li>
+          </ul>
+          {button && <Button buttonStyle="btn--outline">Login</Button>}
+        </div>
+      </nav>
+    </>
+  );
+}
+
+export default Navbar;
